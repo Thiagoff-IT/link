70df6aa297ee83e331ed96bb29cccc1621c09642
Thiago <thiagodsantos316@gmail.com>
2024-11-12 11:05:52 +0000

    update

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { Button, Text, TextInput, View } from 'react-native'
+
+export default function App(){
+    return(
+        <View>
+            <Text>Hello Ecomp!</Text>
+            <TextInput/>
+            <Button 
+                title='Enviar'
+            />
+        </View>
+    )
+}
